47d1cb2aa29412d1ece590999226110b0af6cb1b
reapedjuggler <52465652+reapedjuggler@users.noreply.github.com>
2020-08-23 17:18:35 +0530

    Update quickSort.cpp

--- SORTING TECHNIQUES/quickSort.cpp
+++ SORTING TECHNIQUES/quickSort.cpp
@@ -75,19 +75,3 @@ int main()
         cout << a[i] << " " ;
     return 0 ;
 }
-
-
-{
-    #ifndef ONLINE_JUDGE 
-    freopen("input.txt" , "r" , stdin) ;
-    freopen("output.txt" , "w" , stdout) ;
-    #endif
-}
-
-
-int main()
-{
-  start();
-  
-return 0 ;
-}
